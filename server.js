ca7e3b4a6079f0f90c46c8a831e94228262f6cb0
DilrajDhillon <dhillon.d@husky.neu.edu>
2021-11-25 11:48:16 -0800

    started

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const pokemon = require('./routes/pokemon.js');
+const cors = require('cors')
+const mongoose = require('mongoose');
+
+
+
+//Setup MongoDB Connection
+mongoose.connect('mongodb://127.0.0.1/pokemon_app', { useNewUrlParser: true })
+
+const mongoDB = mongoose.connection;
+
+mongoDB.on('error', console.error.bind(console, 'Error connecting to MongoDB:'));
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+saq`z1  `
+app.use('/api/pokemon', pokemon);  
+// Note that it is common practice got backend APIs in Node to start with the api prefix
+// to distinguish them from frontend routes 
+
+app.get('/', (req, res) => {
+    res.send('NOT BANANA!');
+});
+
+app.listen(8000, function() {
+    console.log('Starting server');
+});
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
+import PokemonDetails from './PokemonDetails';
+import PokemonList from './PokemonList';
+import PokemonSearch from './PokemonSearch';
+import reportWebVitals from './reportWebVitals';
+
+
+
+ReactDOM.render(
+  <Router>
+    <Routes>
+
+      <Route path="/" element={<PokemonList />} />
+      <Route path="/pokemonSearch" element={<PokemonSearch />} />
+      <Route path="/pokemon/:pokemonName" element={<PokemonDetails />} />
+    </Routes>
+  </Router>
+,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
